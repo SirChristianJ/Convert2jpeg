da2ff3789971a5171397806607cd5a3787bef719
SirChristianJ <34753445+SirChristianJ@users.noreply.github.com>
2021-10-14 08:49:52 -0400

    Update convert2jpg.py
    Added line 9 method os.chdir(*Enter directory of test file*) to change the directory to the Downloads folder where the test file was located.

--- convert2jpg.py
+++ convert2jpg.py
@@ -2,16 +2,20 @@
 
 import os
 import sys
+import shutil
 from PIL import Image
 
-if len(sys.argv) > 1:
-    if os.path.exists(sys.argv[1]):
-        im = Image.open(sys.argv[1])
-        target_name = sys.argv[1] + ".jpg"
-        rgb_im = im.convert('RGB')
-        rgb_im.save(target_name)
-        print("Saved as " + target_name)
-    else:
-        print(sys.argv[1] + " not found")
+fn = sys.argv[1]
+os.chdir("../Downloads")
+os.getcwd()
+
+if os.path.exists(fn):  
+    im = Image.open(fn)
+    target_name = fn + ".jpg"
+    rgb_im = im.convert('RGB')
+    rgb_im.save(target_name)
+    print("Saved as " + target_name)
+    os.remove(fn)
+    print("Original file deleted.")
 else:
-    print("Usage: convert2jpg.py <file>")
+    print(fn + " not found")
